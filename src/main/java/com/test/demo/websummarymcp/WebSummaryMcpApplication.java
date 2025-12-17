79bf91475258bbb8e418af0b5122349fee72b2b9
cx22722 <cx22722@outlook.com>
2025-12-17 16:04:54 +0800

    初始化

---
+++ src/main/java/com/test/demo/websummarymcp/WebSummaryMcpApplication.java
@@ -0,0 +1,13 @@
+package com.test.demo.websummarymcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSummaryMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSummaryMcpApplication.class, args);
+	}
+
+}
